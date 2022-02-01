c8855bb914b4d3753a124e2a5a3fbe8f91097e30
GG Tester GG <grishaghlijyan@mail.ru>
2022-02-01 22:26:10 +0400

    Type_conversions

---
+++ Type_conversion/Type_conversion.js
@@ -0,0 +1,23 @@
+
+let myString = "5";
+let numberFromString = Number(myString);
+let bolleanFromString = Boolean(myString);
+
+let num = 40;
+let stringFromNum = String(num);
+let booleanFromNum = Boolean(num);
+
+let myboolean = false;
+let stringFromBooleans = String(myboolean);
+let numFromBoolean = Number(myboolean);
+
+console.log("Converting string to number: " + numberFromString);
+console.log("Converting string to boolean: " + bolleanFromString);
+
+console.log("Converting number to string: " + stringFromNum);
+console.log("Converting number to boolean: " + booleanFromNum);
+
+console.log("Converting boolean to string: " + stringFromBooleans);
+console.log("Converting boolean to number: " + numFromBoolean);
+
+
