16bd11398be1ea4a93fdb699b378a2dd6ee14a87
GGtester01 <grishaghlijyan@mail.ru>
2022-03-02 00:03:35 +0400

    Fruits_task

---
+++ Fruits task/fruits.js
@@ -0,0 +1,20 @@
+let apples;
+let pears;
+function totalFruits(fruit1,fruit2){
+    return("Total amount of fruits is " + (fruit1 + fruit2))
+}
+
+while(true){
+    apples = prompt("Please, write the count of apples: ")
+    pears = prompt("Please, write the count of pears: ");
+    if((parseInt(apples) || apples === "0") && (parseInt(pears) || pears === "0")){
+        apples = Number(apples)
+        pears = Number(pears);
+        alert(totalFruits(apples,pears));
+        break;
+    } else if(apples ===null || pears === null){
+        break;
+    } else{
+        alert("Please, provide numeric value!");
+    }
+}
