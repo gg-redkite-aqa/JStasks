3fc09da269b685702b2afcdb4f4e25c01bdf0dc5
GGtester01 <grishaghlijyan@mail.ru>
2022-04-12 22:16:07 +0400

    Movie Task using forEach

--- Arrays_Tasks/Movies_task.js
+++ Arrays_Tasks/Movies_task.js
@@ -1,8 +1,2 @@
-function getMovieName(movieList){
-    for(i = 0; i < movieList.length; i++){
-        console.log(movieList[i])
-    }
-}
-
 let movies = ["The Avangers", "Inception", "Spider-Man", "Lost and Found"];
-getMovieName(movies)
+movies.forEach(alert)
