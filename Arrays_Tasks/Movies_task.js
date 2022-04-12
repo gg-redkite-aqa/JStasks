017ad506e616d2d836fac14595f417517559a5fb
GGtester01 <grishaghlijyan@mail.ru>
2022-04-12 21:54:58 +0400

    Movie Task

---
+++ Arrays_Tasks/Movies_task.js
@@ -0,0 +1,8 @@
+function getMovieName(movieList){
+    for(i = 0; i < movieList.length; i++){
+        console.log(movieList[i])
+    }
+}
+
+let movies = ["The Avangers", "Inception", "Spider-Man", "Lost and Found"];
+getMovieName(movies)
