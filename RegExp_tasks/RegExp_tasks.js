85f3d1b9af50bddbe1ffb327ce78bfb0214fbdc6
GGtester01 <grishaghlijyan@mail.ru>
2022-05-09 17:00:52 +0400

    Regexp_tasks

---
+++ RegExp_tasks/RegExp_tasks.js
@@ -0,0 +1,8 @@
+
+let str1 = 'ahb acb aeb aeeb adcb axeb';
+let newstr1 = str1.match(/[a][a-z][b]/g);
+alert(newstr1)
+
+let str2 =  '2+3 223 2223';
+let newstr2 = str2.match(/[2][+][3]/g);
+alert(newstr2)
