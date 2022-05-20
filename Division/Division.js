bf3a42cfe035b9edfdcc25fb297dddc1464f4c36
GGtester01 <grishaghlijyan@mail.ru>
2022-05-20 15:31:45 +0400

    Refactoring_division

--- Division/Division.js
+++ Division/Division.js
@@ -1,7 +1,9 @@
-let string1 = "this string";
-let SomeBoolean = false;
-let number1= 400;
 
-alert(string1 / SomeBoolean)
+let string1 = "this string";
+let someBoolean = false;
+let number1 = 400;
+
+alert(string1 / someBoolean)
 alert(string1 / number1)
-alert(number1 / SomeBoolean)
+alert(number1 / someBoolean)
+
