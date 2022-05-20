5eef26f36d0d51c8377938ff301408b4f004d01b
GGtester01 <grishaghlijyan@mail.ru>
2022-05-20 21:22:11 +0400

    refactoring exceptions tasks

--- Exceptions_tasks/exceptionsTasks.js
+++ Exceptions_tasks/exceptionsTasks.js
@@ -1,18 +1,21 @@
-try{
+
+try {
     console.log(a);
     let a = 3;
-}catch(err){
+} catch (err) {
     console.log(err.name + ": let must be defined before usage")
 }
 
-function division(dividend,divisor){
-    try{
-        if(divisor === 0){
+function division(dividend, divisor) {
+    try {
+        if (divisor === 0) {
             throw new Error("Division Error: Can't divide to 0")
-        }console.log(dividend/divisor)
-    }catch(err){
+        }
+        console.log(dividend / divisor)
+    } catch (err) {
         console.log(err.message)
     }
 }
 
-division(7,0)
+division(7, 0)
+
